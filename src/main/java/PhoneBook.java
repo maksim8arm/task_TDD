f2dd145be07e75d4be829862c1ed5c6305f1794a
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-11-17 21:03:59 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,7 +1,17 @@
+import java.util.TreeMap;
+
 public class PhoneBook {
 
+    public static TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+
    public boolean add (String name, String phoneNum){
+       if (!phoneBook.containsKey(name)) {
+           phoneBook.put(name, phoneNum);
+           System.out.println("кол-во контактов: " + phoneBook.size());
+           return true;
+       }
+       System.out.println("Данное имя "+ name +" уже есть в книге");
         return false;
     }
-
 }
+
