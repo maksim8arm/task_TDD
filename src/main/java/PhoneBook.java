0386e17a620c62e75f14e5d759728a46e46147e0
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-11-17 22:18:03 +0300

    Stub and tests for printAllNames

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,20 +1,21 @@
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class PhoneBook {
 
     public static TreeMap<String, String> phoneBook = new TreeMap<String, String>();
 
-   public boolean add (String name, String phoneNum){
-       if (!phoneBook.containsKey(name)) {
-           phoneBook.put(name, phoneNum);
-           System.out.println("кол-во контактов: " + phoneBook.size());
-           return true;
-       }
-       System.out.println("Данное имя "+ name +" уже есть в книге");
+    public boolean add(String name, String phoneNum) {
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, phoneNum);
+            System.out.println("кол-во контактов: " + phoneBook.size());
+            return true;
+        }
+        System.out.println("Данное имя " + name + " уже есть в книге");
         return false;
     }
 
-    public String findByNumber(String phoneNum){
+    public String findByNumber(String phoneNum) {
         for (String key : phoneBook.keySet()) {
             if (phoneNum.equals(phoneBook.get(key))) {
                 return key;
@@ -28,6 +29,10 @@ public class PhoneBook {
             return phoneBook.get(name);
         }
         return "Номер не найден";
+    }
+
+    public ArrayList<String> printAllNames() {
+        return null;
     }
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class PhoneBookTest {
 
@@ -18,21 +21,31 @@ public class PhoneBookTest {
     public void testFindByNumber() {
         PhoneBook phoneBookTest = new PhoneBook();
         phoneBookTest.add("Alex", "123-123");
-        String result =  phoneBookTest.findByNumber("123-123");
+        String result = phoneBookTest.findByNumber("123-123");
         String expected = "Alex";
         Assertions.assertSame(expected, result);
     }
 
     @Test
-    public void findByName(){
+    public void testFindByName() {
         PhoneBook phoneBookTest = new PhoneBook();
         phoneBookTest.add("Alex", "123-123");
-        String result =  phoneBookTest.findByName("Alex");
+        String result = phoneBookTest.findByName("Alex");
         String expected = "123-123";
         Assertions.assertSame(expected, result);
     }
 
-
+    @Test
+    public void testPrintAllNames() {
+        PhoneBook phoneBookTest = new PhoneBook();
+        phoneBookTest.add("Warg", "223-123");
+        phoneBookTest.add("Tom", "333-123");
+        phoneBookTest.add("Bob", "433-123");
+        phoneBookTest.add("Alex", "553-123");
+        ArrayList<String> result = phoneBookTest.printAllNames();
+        ArrayList<String> expected = new ArrayList<String>(Arrays.asList("Alex", "Bob", "Tom", "Warg"));
+        Assertions.assertSame(expected, result);
+    }
 }
 
 
