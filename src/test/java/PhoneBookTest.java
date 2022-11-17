425f3c0f2425c5d09a093d685ce2efa87239010c
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-11-17 21:00:40 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.*;
+
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBookTest = new PhoneBook();
+        String name = "Alex";
+        String phoneNum = "123-45";
+        Boolean result = phoneBookTest.add(name, phoneNum);
+        Boolean expected = true;
+        Assertions.assertSame(expected,result);
+    }
 }
+
+
